8e84a255fb96316904f705110584fb8f735b277b
JYCsec <40046083+JYCSEC@users.noreply.github.com>
2018-06-20 17:16:00 -0400

    Lots of syntax fixes. Program works mostly, need to fill TODOs and format output

--- subdomainhijacking.py
+++ subdomainhijacking.py
@@ -3,21 +3,25 @@ import dns
 from dns import resolver
 
 #TODO Implement as cronjob or persistent python job
-r=resoliver>Resolver
+r=resolver.Resolver
 sites= open("sites.txt","r")
 for line in sites:
     words open("wordlist.txt","r")
         for sd in words:
-            fqdn=words+line
-            a= r.query(fqdn,"A")
-            for i in a:
-                print (i)
-            ns= r.query(fqdn,"NS")
-            for i in ns:
-                print(i)
-            cname= r.query(fqdn,"CNAME")
-            for i in cname:
-                print(i)
+            fqdn=sd.rstrip('\n') + line.rstrip('\n')
+            try:
+                a= r.query(fqdn,"A")
+                cname= r.query(fqdn,"CNAME")
+                print fqdn
+                for i in a:
+                    print i
+                for i in cname:
+                    print i
+            except Exception:
+                pass
+#            ns= r.query(fqdn,"NS")
+#            for i in ns:
+#                print i
 #TODO sort based on page not found & discovery of Github, Heroku, Unbounce, Tumblr, Shopify, Instapage, Desk, Tictail, Campaignmonitor, Cargocollective, Statuspage, Amazonaws, Cloudfront, Bitbucket, Smartling, Acquia, Fastly, Pantheon, Zendesk, Uservoice, Ghost, Freshdesk, Pingdom, Tilda, WordPress, Teamwork, Helpjuice, Helpscout, Cargo, Feedpress, Surge, Surveygizmo, Mashery, Intercom, Webflow, Kajabi, Thinkific, Tave, Wishpond, Aftership, Aha, Brightcove, Bigcartel, Activecompaign, Compaignmonitor, Acquia, Proposify, Simplebooklet, Getresponse, Vend, Jetbrains, Azure
 
 #TODO output to log file with importance rating
